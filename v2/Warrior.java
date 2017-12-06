44d10711f5700f00b09fd37bba2d5fc51d6e6562
bentleydong <bdong1@stuy.edu>
2017-12-05 22:13:09 -0500

    Added toString() to Warrior

--- v2/Warrior.java
+++ v2/Warrior.java
@@ -1,7 +1,12 @@
 public class Warrior extends Protagonist {
+
 	public Warrior(String s) {
 		super(s);
 		strength = 120;
+	}
+
+	public String toString() {
+		return super.toString() + System.lineSeparator() + "Specialized as a Warrior!";
 	}
 	
 }
